994e2c5e15cf2393d72ac800ed7708670829a2f3
Maxim Dmitriev <Renissance@tut.by>
2023-05-07 15:32:51 +0300

    feat:rework loader

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -1,15 +1,24 @@
 import { useAppSelector } from 'common/hooks/hooks'
-import { Loader } from '@mantine/core'
 import { NavLink } from 'react-router-dom'
+import LoadingBar from 'react-top-loading-bar'
 import logo from '../../common/assets/union.svg'
 import Jobored from '../../common/assets/jobored.svg'
 import styles from './Header.module.css'
 
 export const Header = () => {
   const status = useAppSelector(state => state.app.status)
+
   return (
     <div className={styles.container}>
-      {status === 'loading' && <Loader className={styles.loader} variant='dots' />}
+      {status === 'loading' && (
+        <LoadingBar
+          height={5}
+          progress={100}
+          transitionTime={6000000}
+          color={'#5E96FC'}
+          loaderSpeed={1000}
+        />
+      )}
       <div className={styles.logo}>
         <img src={logo} alt='alt' />
         <img className={styles.text} src={Jobored} alt='alt' />
