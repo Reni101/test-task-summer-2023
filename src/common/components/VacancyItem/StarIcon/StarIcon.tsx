bb76818db295f69520c55c65167bbd3d95397e0d
Maxim Dmitriev <Renissance@tut.by>
2023-05-07 16:51:54 +0300

    feat:add disabled icon

--- src/common/components/VacancyItem/StarIcon/StarIcon.tsx
+++ src/common/components/VacancyItem/StarIcon/StarIcon.tsx
@@ -3,6 +3,7 @@ import { Star } from 'tabler-icons-react'
 import { IVacancy } from 'features/searchVacancies/searchVacancies.api'
 import { useAppDispatch, useAppSelector } from 'common/hooks/hooks'
 import { toggleFavorite } from 'features/favorite/favorite.slice'
+import { ActionIcon } from '@mantine/core'
 
 interface PropsType {
   vacancy: IVacancy
@@ -12,18 +13,26 @@ export const StarIcon: FC<PropsType> = memo(({ vacancy }) => {
   const dispatch = useAppDispatch()
   const favVacancies = useAppSelector(state => state.favorite)
   const isFavorite = favVacancies.some(el => el.id === vacancy.id)
-
+  const status = useAppSelector(state => state.app.status)
   return (
     <div
       onClick={() => {
         dispatch(toggleFavorite(vacancy))
       }}
     >
-      <Star
-        style={{ cursor: 'pointer' }}
-        color={isFavorite ? '#5E96FC' : '#ACADB9'}
-        fill={isFavorite ? '#5E96FC' : '#fff'}
-      />
+      <ActionIcon
+        variant='transparent'
+        sx={{
+          '&[data-disabled]': { opacity: 0.5 }
+        }}
+        disabled={status === 'loading'}
+      >
+        <Star
+          style={{ cursor: 'pointer' }}
+          color={isFavorite ? '#5E96FC' : '#ACADB9'}
+          fill={isFavorite ? '#5E96FC' : '#fff'}
+        />
+      </ActionIcon>
     </div>
   )
 })
