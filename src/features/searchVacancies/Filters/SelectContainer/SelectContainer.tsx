02fb49161ac341b67e9aa26e4003f2c654842a80
Maxim Dmitriev <Renissance@tut.by>
2023-05-18 01:37:45 +0300

    feat:refactor css

--- src/features/searchVacancies/Filters/SelectContainer/SelectContainer.tsx
+++ src/features/searchVacancies/Filters/SelectContainer/SelectContainer.tsx
@@ -4,12 +4,15 @@ import { useAppDispatch, useAppSelector } from 'common/hooks/useAppHooks'
 import { selectCatalogs } from 'features/searchVacancies/Filters/catalogs.slice'
 import { selectCategory } from 'features/searchVacancies/searchVacancies.selectors'
 import { ArrowIcon } from 'common/assets/icons/ArrowIcon'
+import { useState } from 'react'
 import styles from './SelectContainer.module.scss'
 
 export const SelectContainer = () => {
   const dispatch = useAppDispatch()
   const catalogs = useAppSelector(selectCatalogs)
   const categoryState = useAppSelector(selectCategory)
+
+  const [isOpenMenu, setIsOpenMenu] = useState(false)
 
   const dataForSelect = catalogs.map(catalog => ({
     value: catalog.key.toString(),
@@ -26,15 +29,23 @@ export const SelectContainer = () => {
       <Select
         data-elem='industry-select'
         placeholder='Выберете отрасль'
+        onDropdownOpen={() => setIsOpenMenu(true)}
+        onDropdownClose={() => setIsOpenMenu(false)}
         searchable
         clearable
         classNames={{
-          input: styles.selectContainer,
+          input: styles.selectInput,
           rightSection: styles.rightSection,
           dropdown: styles.dropdown,
           item: styles.item
         }}
-        rightSection={<ArrowIcon width={'15px'} height={'8px'} />}
+        rightSection={
+          <ArrowIcon
+            className={isOpenMenu ? styles.open : undefined}
+            width={'15px'}
+            height={'8px'}
+          />
+        }
         onChange={changeCategoryHandler}
         rightSectionWidth={40}
         value={categoryState}
