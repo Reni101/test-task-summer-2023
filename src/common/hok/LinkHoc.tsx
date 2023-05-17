e139baabf7294f4fc1934f082b55c71defc67630
Maxim Dmitriev <Renissance@tut.by>
2023-05-18 00:29:48 +0300

    feat:refactor code

---
+++ src/common/hok/LinkHoc.tsx
@@ -0,0 +1,20 @@
+import { FC, ReactNode } from 'react'
+import { NavLink } from 'react-router-dom'
+interface PropsType {
+  children: ReactNode
+  href: string | null
+  className: string
+  vacancyId: number
+}
+
+export const LinkHoc: FC<PropsType> = ({ children, href, className, vacancyId }) => {
+  if (href) {
+    return (
+      <NavLink data-elem={`vacancy-${vacancyId}`} className={className} to={href}>
+        {children}
+      </NavLink>
+    )
+  } else {
+    return <div className={className}>{children}</div>
+  }
+}
